3587f5dadd964262681d991d29fa20fbf0d04352
krisu <kdemczynski@wp.pl>
2016-10-17 18:13:54 +0200

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,13 @@
+glosnosc = 57
+if glosnosc < 20:
+    print("Prawie nic nie slychac.")
+elif 20 <= glosnosc < 40:
+    print("O, muzyka leci w tle.")
+elif 40 <= glosnosc < 60:
+    print("Idealnie, moge uslyszec wszystkie detale")
+elif 60 <= glosnosc < 80:
+    print("Dobre na imprezy")
+elif 80 <= glosnosc < 100:
+    print("Troszeczke za glosno!")
+else:
+    print("Ojoj! Moje uszy! :(")
